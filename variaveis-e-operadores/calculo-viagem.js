5155d3cb55d272698dd1fe757f9d413d221b02d5
AlexsanderGS <alexsanderga@gmail.com>
2025-07-22 20:21:25 -0300

    Exercicio Calculo de Viagem

---
+++ variaveis-e-operadores/calculo-viagem.js
@@ -0,0 +1,8 @@
+const precoCombustivel = 6.43;
+const combustivelPorKm = 16;
+let distanciaEmKm = 500;
+
+let litrosConsumidos = distanciaEmKm / combustivelPorKm;
+let consumoCombustivel = litrosConsumidos * precoCombustivel;
+
+console.log('O custo para realizar essa viagem será de: R$ ' + consumoCombustivel.toFixed(2));
