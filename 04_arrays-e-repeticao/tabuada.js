61414d05a4ea76fdd1e8a8f49be1a44492b77486
AlexsanderGS <alexsanderga@gmail.com>
2025-08-07 20:40:56 -0300

    Realizando o exercicio de uma tabuada com estruturas de repetição

---
+++ 04_arrays-e-repeticao/tabuada.js
@@ -0,0 +1,11 @@
+
+// 1 -  Crie um programa que dado um número imprima sua tabuada.
+
+let numero = 7;
+
+console.log('Tabuada do número inserido: '); 
+
+for (let i = 1; i <= 10; i++) {
+    let tabuada = i * numero; 
+    console.log(tabuada);
+}
